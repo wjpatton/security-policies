ecb495fce5b28a518c9fb7b303ef5c62056c3eb2
William Patton <wjpatton@wjpatton.com>
2019-10-01 10:42:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "tfe_policy_set" "global" {
     "${tfe_sentinel_policy.aws-block-allow-all-cidr.id}",
     "${tfe_sentinel_policy.aws-restrict-instance-type-default.id}",
     "${tfe_sentinel_policy.aws-vpc-must-be-pmr-approved.id}",
+    "${tfe_sentinel_policy.limit-cost-by-workspace-type.id}",
   ]
 }
 
